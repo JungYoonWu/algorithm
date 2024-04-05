5d68cce980d7692f49a4e097221da293bc9bdcc9
JungYoonWu <163228965+JungYoonWu@users.noreply.github.com>
2024-04-05 15:57:10 +0900

    Add files via upload

---
+++ stack/IntStack.java
@@ -0,0 +1,62 @@
+package stack;
+
+public class IntStack {
+	private int[] stack;
+	private int top;		//top은 stack에서 값이 있는곳 바로 다음 index 자료가 차있는 갯수
+	private int capacity;
+	
+	IntStack(int capacity){		//capacity 값으로 stack생성자
+		this.capacity = capacity;
+		this.stack = new int[capacity];
+	}
+	
+	public boolean push(int num) {
+		if(full()) {return false;}
+		
+		stack[top++] = num;
+		return true;
+	}
+	
+	public boolean full() {
+		return top == capacity;
+	}
+	
+	public int pop() {
+		if(empty()) {return -1;}
+		return stack[--top];
+	}
+	
+	public boolean empty() {
+		return top==0;
+	}
+	
+	public int top() {//top-1에 있는 데이터를 보여줌 
+		if(empty()) {
+			return -1;
+		}
+		return stack[top-1];
+	}
+	
+	public int size() {
+		return top;
+	}
+	
+	public int capacity() {
+		return capacity;
+	}
+	
+	public void clear() {
+		this.top = 0;
+	}
+	
+	public void dump() {
+		if(empty()) {
+			System.out.println("스택이 비어 있습니다.");
+		}else {
+			for(int i = 0; i < top; i++) {
+				System.out.print(stack[i] + " ");
+			}System.out.println();
+		}
+		
+	}
+}
